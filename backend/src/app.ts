574e05de08858fb794d4b161e36b130acfe226de
Denis <deni4gt@gmail.com>
2026-09-21 14:03:35 +0300

    fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,7 +20,7 @@ app.set('trust proxy', 1);
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173', "https://app.lifedesk.me"],
     credentials: true,
   }),
 );
